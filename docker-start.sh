85d30e87dd2e143951fe18c155e06f64151b123b
tttsss-01 <110894574+tttsss-01@users.noreply.github.com>
2024-07-10 11:07:39 +0800

    Add files via upload

---
+++ docker-start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ ! -d /resources/StarRailData ]; then
+    mkdir -p /resources/StarRailData &&
+        curl -L https://github.com/Dimbreath/StarRailData/archive/master.tar.gz | tar -xz --strip-components=1 -C /resources/StarRailData
+fi
+
+if [ ! -d /resources/LunarCore-Configs ]; then
+    mkdir -p /resources/LunarCore-Configs &&
+        curl -L https://gitlab.com/Melledy/LunarCore-Configs/-/archive/main/LunarCore-Configs-main.tar.gz | tar -xz --strip-components=1 -C /resources/LunarCore-Configs
+fi
+
+mkdir -p /LunarCore/resources
+cp -rf /resources/StarRailData/* /LunarCore/resources/
+cp -rf /resources/LunarCore-Configs/* /LunarCore/resources/
+
+echo "finish copying resource"
+
+java -jar /LunarCore/LunarCore.jar
